a4a7bb8e47266da153855636a1f75da17a9c65f1
Marcin <marcin.barabasz@gmail.com>
2014-08-16 11:18:25 +0200

    settings

---
+++ KindleTurn/src/pl/whipsoft/kindleturn/SettingsActivity.java
@@ -0,0 +1,13 @@
+package pl.whipsoft.kindleturn;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+	}
+}
